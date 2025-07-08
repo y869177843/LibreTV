d39b7d8e3c9b5869506d375204cd75b7a0bd80a3
y869177843 <45436453+y869177843@users.noreply.github.com>
2025-07-08 20:28:17 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -15,7 +15,7 @@ const PASSWORD_CONFIG = {
 const SITE_CONFIG = {
     name: 'LibreTV',
     url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    description: '不让看 不让你看 就不让你看 大乐不许看',
     logo: 'image/logo.png',
     version: '1.0.3'
 };
